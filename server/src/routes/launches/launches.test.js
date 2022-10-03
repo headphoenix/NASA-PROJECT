d116b8e5af4a95066235540c8c814fb9216713fb
Kofi <otofitatu@gmail.com>
2022-10-03 20:54:57 +0000

    Finished Server & added Jest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,71 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+
+describe("Test POST /Launch", () => {
+  const completeLaunchData = {
+    mission: "Hello",
+    rocket: "StarLink",
+    target: "Kepler 1046",
+    launchDate: "January 6, 2023",
+  };
+
+  const launchDataWithoutDate = {
+    mission: "Hello",
+    rocket: "StarLink",
+    target: "Kepler 1046",
+  };
+
+  const launchDataWithInvalidDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-62 f",
+    launchDate: "zoot",
+  };
+
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
+  });
+
+  test("It should catch invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date",
+    });
+  });
+});
